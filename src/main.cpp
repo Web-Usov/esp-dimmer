779eed72283ceb8b5cf9fb534af05270d0b48846
Усов Алексей <web.usov@ya.ru>
2026-08-29 17:09:00 +0400

    refactor: pass button polarity per channel

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,15 @@ namespace {
 
 DimmerChannel channels[config::kChannelCount] = {
     {1, config::kLedPins[0], config::kButtonPins[0],
-     config::kButtonInternalPullup[0]},
+     config::kButtonInternalPullup[0], config::kButtonActiveHigh[0]},
     {2, config::kLedPins[1], config::kButtonPins[1],
-     config::kButtonInternalPullup[1]},
+     config::kButtonInternalPullup[1], config::kButtonActiveHigh[1]},
     {3, config::kLedPins[2], config::kButtonPins[2],
-     config::kButtonInternalPullup[2]},
+     config::kButtonInternalPullup[2], config::kButtonActiveHigh[2]},
     {4, config::kLedPins[3], config::kButtonPins[3],
-     config::kButtonInternalPullup[3]},
+     config::kButtonInternalPullup[3], config::kButtonActiveHigh[3]},
     {5, config::kLedPins[4], config::kButtonPins[4],
-     config::kButtonInternalPullup[4]},
+     config::kButtonInternalPullup[4], config::kButtonActiveHigh[4]},
 };
 
 }  // namespace
